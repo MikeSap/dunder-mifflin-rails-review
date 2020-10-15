199fe7526ae4d7e81c6d7d4e01650d42e60a99af
mike <mikesapienzaaudio@gmail.com>
2020-10-15 08:44:08 -0500

    no bonus

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,6 @@
 class Dog < ApplicationRecord
     has_many :owners, class_name: "Employee", foreign_key: "dog_id"
+
+
+
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,11 +1,8 @@
 class Employee < ApplicationRecord
     belongs_to :dog
 
-    validates :alias, uniqueness: true
-    validates :title, uniqueness: true
-
-
-
+    validates :last_name, :alias, :title, uniqueness: true
+    #how do we validate first and last name together?
     def name
         self.first_name + " " + self.last_name
     end
